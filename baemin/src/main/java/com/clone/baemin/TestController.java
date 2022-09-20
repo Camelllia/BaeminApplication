0f400f6833ddebc6ca463dad13eee6734a8b056f
KimSeungJin <dahyun101107@naver.com>
2022-09-20 20:20:47 +0900

    Project Setting

---
+++ baemin/src/main/java/com/clone/baemin/TestController.java
@@ -0,0 +1,13 @@
+package com.clone.baemin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+
+    @RequestMapping(value = "/")
+    public String test() {
+        return "test";
+    }
+}
