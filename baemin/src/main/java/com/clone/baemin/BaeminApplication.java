48bb90cc2a8111744eb1b6a124c75b0aa2b09be3
KimSeungJin <dahyun101107@naver.com>
2022-09-21 20:53:09 +0900

    test

--- baemin/src/main/java/com/clone/baemin/BaeminApplication.java
+++ baemin/src/main/java/com/clone/baemin/BaeminApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BaeminApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BaeminApplication.class, args);
 	}
-
 }
