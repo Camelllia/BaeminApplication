91ff596421a4149f0d40229aae009095c5583d35
KimSeungJin <dahyun101107@naver.com>
2022-09-22 15:46:08 +0900

    Create : User DB Table

--- baemin_db_create .sql
+++ baemin_db_create .sql
@@ -1,8 +1,23 @@
 -- 유저 로그인 이력
 CREATE TABLE baemin_db.user_login_log (
  logIdn      INT NOT NULL AUTO_INCREMENT,
- email     VARCHAR(50),
- regIp VARCHAR(50),
- regDate   DATETIME,
+ userEmail     VARCHAR(100) NOT NULL,
+ regIp VARCHAR(100) NOT NULL,
+ regDate   DATETIME NOT NULL DEFAULT NOW(),
   PRIMARY KEY(logIdn)
 ) ENGINE=MYISAM CHARSET=utf8;
+
+-- 유저 정보
+CREATE TABLE baemin_db.user_info (
+ userIdn      INT NOT NULL AUTO_INCREMENT,
+ userNickname     VARCHAR(100) NOT NULL,
+ userEmail     VARCHAR(100) NOT NULL,
+ userPw     VARCHAR(100) NOT NULL,
+ userPhonenum    VARCHAR(100) NOT NULL,
+ userPoint    INT(11),
+ regIp VARCHAR(100) NOT NULL,
+ regDate   DATETIME NOT NULL DEFAULT NOW(),
+  PRIMARY KEY(userIdn)
+) ENGINE=MYISAM CHARSET=utf8;
+
+
