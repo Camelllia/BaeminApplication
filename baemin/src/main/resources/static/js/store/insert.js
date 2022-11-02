ac220be2e74aced089905b9d7000b34bc1d3ca05
KimSeungJin <dahyun101107@naver.com>
2022-11-02 14:25:23 +0900

    스크립트 분리

---
+++ baemin/src/main/resources/static/js/store/insert.js
@@ -0,0 +1,70 @@
+var createStore = function() {
+
+    var storeName = $("#storeName").val();
+    var storeAddress = $("#storeAddress").val();
+    var storePhonenum = $("#storePhonenum").val();
+    var storeIntro = $("#storeIntro").val();
+    var deleveryTime = $("#deleveryTime").val();
+    var deleveryTip = $("#deleveryTip").val();
+    var minDelevery = $("#minDelevery").val();
+    var categoryNum = $("#categoryState option:selected").val();
+    var imgFile = $("#imgFile")[0].files[0];
+
+
+    if(storeName == "" || storeAddress == "" || storePhonenum == "" || storeIntro == "" || deleveryTime == "" || deleveryTip == "" || minDelevery == "" || categoryNum == "") {
+        alert("입력되지 않은 내용이 있습니다.");
+        return;
+    }
+
+    if(imgFile == undefined) {
+        alert("매장 사진을 등록해주세요");
+        return;
+    }
+
+    var formData = new FormData();
+
+    formData.append("storeName", storeName);
+    formData.append("storeAddress", storeAddress);
+    formData.append("storePhonenum", storePhonenum);
+    formData.append("storeIntro", storeIntro);
+    formData.append("deleveryTime", deleveryTime);
+    formData.append("deleveryTip", deleveryTip);
+    formData.append("minDelevery", minDelevery);
+    formData.append("categoryNum", categoryNum);
+    formData.append("imgFile", imgFile);
+    formData.append("fileName", imgFile.name);
+
+    $.ajax({
+        type:"POST",
+        url:"/createStore",
+        data:formData,
+        contentType : false,
+        processData : false,
+        success:function(response) {
+
+            const result = JSON.parse(response);
+
+            if(result.resultCode == "1") {
+                alert("정상적으로 등록되었습니다.");
+                location.href = location.href;
+            } else if(result.resultCode == "-20") {
+                alert("입력되지 않은 내용이 있습니다.");
+            } else if(result.resultCode == "-30") {
+                alert("사진은 PNG 형식으로 등록해주세요.");
+            }
+        },
+        error:function (err) {
+            console.log(err);
+            alert("매장 등록에 실패하였습니다.");
+        }
+    })
+}
+
+var selectCategory = function() {
+    var categoryState = $("#categoryState option:selected").text();
+    if(categoryState == '선택') {
+        $("#selectCategoryName").val("카테고리를 선택해주세요");
+    } else {
+        $("#selectCategoryName").val(categoryState);
+    }
+}
--- baemin/src/main/webapp/WEB-INF/views/store/detail.jsp
+++ baemin/src/main/webapp/WEB-INF/views/store/detail.jsp
@@ -65,43 +65,6 @@
 		position: markerPosition
 	});
 
-	var insertBasket = function(index) {
-		var storeIdn = $("#storeIdn").val();
-		var menuName = $("#menuName"+index).html();
-		var menuPrice = $("#menuPrice"+index).val();
-		
-		var param = {
-			menuName : menuName,
-			menuPrice : menuPrice,
-			storeIdn : storeIdn
-		}
-
-		if(menuName == '' || menuPrice == '' || storeIdn == '') {
-			alert("정보가 유효하지 않습니다.");
-			return;
-		}
-
-		$.ajax({
-            type:"POST",
-            url:"/insertBasket",
-            data:param,
-            success:function(response) {
-			
-                const result = JSON.parse(response);
-				
-                if(result.resultCode == "1") {
-                	alert("상품을 담았습니다.");
-				} else if(result.resultCode == "-10"){
-					alert("정보가 유효하지 않습니다.");
-                    return;
-				}
-            },
-            error:function (err) {
-                alert("장바구니 담기에 실패하였습니다.");
-            }
-        })
-	}
-
 	// 마커가 지도 위에 표시되도록 설정합니다
 	marker.setMap(map);
 </script>
